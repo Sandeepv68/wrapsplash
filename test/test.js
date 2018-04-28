11f1a9c57b309094493105bdda326a8c5287d42f
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-04-28 20:38:47 +0530

    edits

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const UnsplashApi = require('../build/main');
+const UnsplashApi = require('../dist/main');
 
 unsplash = new UnsplashApi.WrapSplashApi({
     access_key: '5a419933621f55950741e4c30182c1d5f99974112a96cf0c49d8d9091cc0ba7e',
