4a65404f5af15f4cf3ce863de8ee778c600173a9
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-05-01 22:37:25 +0530

    chai test passing

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,28 +1,58 @@
-const chai = require('chai');
-const WrapASplashAPI = require('../dist/main');
+/**
+ * Chaijs test.spec file for unit tests
+ * written by: Sandeep Vattapparambil
+ */
 
-chai.expect();
+//import chaijs
+import chai from 'chai';
+//import WrapSplashJS
+import WrapASplashAPI from '../dist/main';
+
+//set the Expect Assertion
 const expect = chai.expect;
+//set the Should Assertion
+const should = chai.should;
 
+//define the UnsplashAPI object
 let UnsplashAPI;
-describe('Given an instance of WraspSplashJS', () => {
 
+//describe the test function
+describe('Given an instance of WrapSplashJS', () => {
+    //setup and initialize the UnsplashAPI object with the required parameters
     beforeEach(() => {
         UnsplashAPI = new WrapASplashAPI({
-            access_key: '5a419933621f55950741e4c30182c1d5f99974112a96cf0c49d8d9091cc0ba7e',
-            secret_key: '0464eb09a8590dfaaf11e562a8c7a361c15d25d7757f26e20e6b86d0fd6b9c5c',
+            access_key: '50f4e0688dedca12f966ea044221acf610e2ce4b47a24b4b34c0d34f8ac79cdb',
+            secret_key: '6e4e5f1bb0b0d9987ff763907345b0b87281c1a2273daf4643f5f0a0c6c7b2da',
             redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
-            code: '172112daa24a80945e9c15b8ee6e21e60f83337868fc33b439326d71f041009f',
-            bearer_token: '2c15444720fd795bec49aa23fd3982722626377471ec29af581d5d0f07da565c'
+            code: '53697a83edbd3039a0a1e4abf181514da1d2ab39008c0eec33dfcbd6739d749b',
+            bearer_token: '2175e61a37bce90fd86fbfa759384ad8a22c88f25ffbc45db17d5a36d731ae10'
         });
     });
 
+    //describe the unit test
     describe('When the function -> listPhotos() is called without parameters', () => {
         it('It should return the Array of list of photos from Unsplash API', () => {
             UnsplashAPI.listPhotos().then((result) => {
                 expect(result).to.be.a('array');
             });
         });
+        it('It should return the result array having length between 0 - 10', () => {
+            UnsplashAPI.listPhotos().then((result) => {
+                expect(result).to.have.length.within(0, 10);
+            });
+        });
     });
 
+    describe('When the function -> listPhotos() is called with parameters page = 1 & per_page = 20', () => {
+        it('It should return the Array of list of photos from Unsplash API', () => {
+            UnsplashAPI.listPhotos(1, 20, 'latest').then((result) => {
+                expect(result).to.be.a('array');
+            });
+        });
+        it('It should return the result array having length between 0 - 20', () => {
+            UnsplashAPI.listPhotos(1, 20, 'latest').then((result) => {
+                expect(result).to.have.length.within(0, 20);
+            });
+        });
+    });
 });
